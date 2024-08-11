c536ad6dececc5bc6225acd856ed54d887218131
[DongDongLab] <kdwport@naver.com>
2024-08-12 08:54:28 +0900

    show todo list

---
+++ src/components/TodoList/TodoList.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+export default function Todolist() {
+  const [todos, setTodos] = useState([
+    { id: '123', text: '장보기', status: 'active' },
+    { id: '124', text: '공부하기', status: 'active' },
+  ]);
+  return (
+    <section>
+      <ul>
+        {todos.map((item) => (
+          <li key={item.id} status={item.status}>
+            {item.text}
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
